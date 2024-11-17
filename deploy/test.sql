ca8bfe9bbb5fe025d2785e7a1c8815904fbbc231
zahraaahojati <z1hojati@gmail.com>
2024-11-17 13:26:14 -0700

    Update test.sql

--- deploy/test.sql
+++ deploy/test.sql
@@ -3,10 +3,7 @@ BEGIN;
 SET client_min_messages = 'warning';
 
 CREATE TABLE cards.test (
-  SELECT 
-    'cmc' AS CmC
-FROM 
-    scryfall_cards;
+  ickname  TEXT        PRIMARY KEY,
 );
 
 COMMIT;
